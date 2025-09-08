c85aee910bed4c6d484b182a93e9b84cb3bd8120
wilquepictures <florence.wilque@hotmail.com>
2025-09-08 13:51:44 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'nutriscan-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/serviceworker.js',
+  // Voeg hier de paden naar je CSS-bestanden, JavaScript-bestanden en afbeeldingen toe
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
